6168b03bbb90fa4127ec7981d538fc417eab6bb5
Timothy-Smeers <timothy.smeers@student.uliege.be>
2021-05-08 10:01:06 +0200

    modify list_race

--- src/list_race.c
+++ src/list_race.c
@@ -26,8 +26,40 @@ struct Course_t {
 
 /* DYNAMIC FUNCTION */
 
-Cell* create_cell(Escale *p_data) {
-	assert(p_data != NULL);
+Course* create_race(Escale *p_stopover, Escale *p_secondStopover) {
+	assert(p_stopover != NULL);
+	assert(p_secondStopover != NULL);
+
+	Course *l_race;
+
+	l_race = NULL;
+
+	add_end(&l_race, p_stopover);
+	add_end(&l_race, p_secondStopover);
+
+	return (l_race);
+}
+
+Escale* obtain_stopover(Course *p_race, Escale *p_stopover) { /* @suppress("No return") */
+	assert(p_race != NULL);
+	assert(p_stopover != NULL);
+
+	Cell *l_cell;
+
+	l_cell = p_race;
+
+	while (l_cell != NULL) {
+
+		if (l_cell->s_stopover == p_stopover)
+			return (l_cell->s_stopover);
+
+		l_cell = l_cell->s_next_stopover;
+	}
+}
+
+void* add_start(Course **p_race, Escale *p_stopover) { /* @suppress("No return") */
+	assert(p_race != NULL);
+	assert(p_stopover != NULL);
 
 	Cell *l_cell;
 
@@ -36,226 +68,153 @@ Cell* create_cell(Escale *p_data) {
 	if (l_cell == NULL)
 		return (NULL);
 
-	l_cell->s_data = p_data;
-	l_cell->s_next = NULL;
+	l_cell->s_stopover = p_stopover;
+	l_cell->s_next_stopover = *p_race;
 
-	return (l_cell);
+	*p_race = l_cell;
 }
 
-static Course* add_start(Course *p_header, Escale *p_stopover) {
-	assert(p_header != NULL);
+void* add_end(Course **p_race, Escale *p_stopover) { /* @suppress("No return") */
 	assert(p_stopover != NULL);
 
 	Cell *l_cell;
+	Cell *l_lastCell;
 
-	l_cell = create_cell(p_stopover);
+	l_lastCell = *p_race;
+	l_cell = malloc(sizeof(Cell));
 
 	if (l_cell == NULL)
 		return (NULL);
 
-	l_cell->s_next = p_header->s_start;
+	l_cell->s_stopover = p_stopover;
+	l_cell->s_next_stopover = NULL;
 
-	if (p_header->s_end == p_header->s_start)
-		p_header->s_end->s_next = l_cell;
-
-	p_header->s_start = l_cell;
-	p_header->s_nbrStopover += 1;
-
-	return (p_header);
-}
-
-static Course* add_end(Course *p_header, Escale *p_stopover) {
-	assert(p_header != NULL);
-	assert(p_stopover != NULL);
-
-	Cell *l_cell;
-
-	l_cell = create_cell(p_stopover);
-
-	if (l_cell == NULL)
-		return (NULL);
-
-	l_cell->s_next = p_header->s_end->s_next;
-	p_header->s_end->s_next = l_cell;
-	p_header->s_end = l_cell;
-	p_header->s_nbrStopover += 1;
-
-	return (p_header);
-}
-
-Course* create_race(Escale *p_stopover, Escale *p_secondStopover) {
-	assert(p_stopover != NULL);
-	assert(p_secondStopover != NULL);
-
-	Cell *l_race;
-	Course *l_header;
-
-	l_race = create_cell(p_stopover);
-	l_header = malloc(sizeof(Course));
-
-	if (l_header == NULL)
-		return (NULL);
-
-	l_race->s_next = create_cell(p_secondStopover);
-	l_race->s_next->s_next = NULL;
-
-	l_header->s_start = l_race;
-	l_header->s_end = l_race->s_next;
-	l_header->s_nbrStopover = 2;
-
-	return (l_header);
-}
-
-Course* add_stopover(Course *p_race, Escale *p_stopover, int p_position) {
-	assert(p_race != NULL);
-	assert(p_stopover != NULL);
-	assert(p_position > 0);
-
-	unsigned int l_number;
-	Course l_start;
-	Course l_end;
-	Cell *l_current;
-	Cell *l_previous;
-	Cell *l_cell;
-
-	l_number = get_stopover(p_race);
-	l_start = add_start(p_race, p_stopover);
-	l_end = add_end(p_race, p_stopover);
-
-	if (p_position > l_number)
-		return (NULL);
-
-	if (p_position == 1)
-		return (l_start);
-
-	if (p_position == l_number)
-		return (l_end);
-
-	l_current = p_race->s_start;
-	l_previous = NULL;
-
-	for (unsigned int i = 1; i < p_position; i++) {
-		l_previous = l_current;
-		l_current = l_current->s_next;
-	}
-
-	l_cell = create_cell(p_stopover);
-
-	if (l_cell == NULL)
-		return (NULL);
-
-	l_cell->s_next = l_current;
-	l_previous->s_next = l_cell;
-	p_race->s_nbrStopover += 1;
-
-	return (p_race);
-}
-
-Course* remove_stopover(Course *p_race, int p_position) {
-	assert(p_race != NULL);
-	assert(p_position > 0);
-
-	unsigned int l_number;
-	Course *l_remove;
-
-	l_number = get_stopover(p_race);
-
-	if (p_position > l_number)
-		return (NULL);
-
-	p_race->s_count++;
-
-	if (p_position == 1) {
-		p_race->s_count = 0;
-		p_race->s_pointer = p_race->s_start;
-		p_race->s_start = p_race->s_pointer->s_next;
-		p_race->s_end->s_next = p_race->s_pointer->s_next;
-		free(p_race->s_pointer);
-		p_race->s_nbrStopover -= 1;
-
-		return (p_race);
-	}
-
-	if (p_race->s_count == 1)
-		p_race->s_pointer = p_race->s_start;
-
-	if (p_race->s_count > 1) {
-		p_race->s_nullPointer = p_race->s_pointer;
-		p_race->s_pointer = p_race->s_pointer->s_next;
-	}
-
-	if (p_race->s_count == p_position) {
-
-		if (p_position == l_number) {
-			p_race->s_end = p_race->s_nullPointer;
-		}
-		p_race->s_count = 0;
-		p_race->s_nullPointer->s_next = p_race->s_pointer->s_next;
-
-		free(p_race->s_pointer);
-		p_race->s_nbrStopover -= 1;
-
-		return (p_race);
-	}
-
-	l_remove = remove_stopover(p_race, p_position);
-
-	return (l_remove);
-}
-
-Escale* obtain_stopover(Course *p_race, int p_position) {
-	assert(p_race != NULL);
-
-	unsigned int l_number;
-	Cell *l_cell;
-
-	l_number = get_stopover(p_race);
-	l_cell = p_race->s_pointer;
-
-	if (p_position > l_number)
-		return (NULL);
-
-	if (p_race->s_count == 0) {
-		l_cell = p_race->s_start;
-
-		for (unsigned int i = 1; i <= p_position; i++)
-			l_cell = l_cell->s_next;
+	if (*p_race == NULL) {
+		*p_race = l_cell;
+		l_cell->s_nbrStopover = 1;
 
 	} else {
 
-		for (unsigned int i = p_race->s_count; i < p_position; i++)
-			l_cell = l_cell->s_next;
+		while (l_lastCell->s_next_stopover != NULL)
+			l_lastCell = l_lastCell->s_next_stopover;
+
+		l_lastCell->s_next_stopover = l_cell;
 	}
+}
 
-	l_number = get_stopover(p_race);
+void remove_stopover(Course **p_race, Escale *p_stopover) {
+	assert(p_race != NULL);
+	assert(p_stopover != NULL);
 
-	if (p_race->s_count == l_number)
-		p_race->s_count = 0;
+	Cell *l_cell;
+	Cell *l_current;
 
-	return (l_cell->s_data);
+	if ((*p_race)->s_stopover == p_stopover) {
+		l_cell = *p_race;
+		(*p_race) = (*p_race)->s_next_stopover;
+		free(l_cell);
+
+	} else {
+		l_current = *p_race;
+
+		while (l_current->s_next_stopover != NULL) {
+
+			if (l_current->s_next_stopover->s_stopover == p_stopover) {
+				l_cell = l_current->s_next_stopover;
+				l_current->s_next_stopover =
+						l_current->s_next_stopover->s_next_stopover;
+				free(l_cell);
+				break;
+
+			} else {
+				l_current = l_current->s_next_stopover;
+			}
+		}
+	}
+}
+
+void print_race(Course *p_race) {
+	assert(p_race != NULL);
+
+	char *l_name;
+	Cell *l_cell;
+
+	l_name = get_name(p_race->s_stopover);
+	l_cell = p_race;
+
+	while (l_cell != NULL) {
+		printf("\n %s", l_name);
+		l_cell = l_cell->s_next_stopover;
+	}
 }
 
 void free_race(Course *p_race) {
 	assert(p_race != NULL);
 
-	p_race->s_pointer = p_race->s_start;
-	unsigned int l_number = get_stopover(p_race);
+	Cell *l_cell;
 
-	if (p_race->s_pointer->s_next != NULL) {
-
-		if (p_race->s_pointer->s_next != p_race->s_start) {
-
-			for (unsigned int i = 1; i < l_number; i++) {
-				free_stopover(p_race->s_pointer->s_data);
-				free(p_race->s_pointer);
-				p_race->s_pointer = p_race->s_pointer->s_next;
-			}
-		}
-	} else {
-		free_stopover(p_race->s_pointer->s_data);
-		free(p_race->s_pointer);
-		free(p_race);
+	while (p_race != NULL) {
+		l_cell = p_race;
+		p_race = p_race->s_next_stopover;
+		free(l_cell);
 	}
+}
+
+float stopover_race_time(Course *p_race, Escale *p_stopover) { /* @suppress("No return") */
+	assert(p_race != NULL);
+	assert(p_stopover != NULL);
+
+	float l_time;
+	Cell *l_cell;
+
+	l_cell = p_race;
+
+	while (l_cell != NULL) {
+
+		l_time = get_best_time(l_cell->s_stopover);
+
+		if (l_cell->s_stopover == p_stopover)
+			return (l_time);
+
+		l_cell = l_cell->s_next_stopover;
+	}
+}
+
+float race_time(Course *p_race) {
+	assert(p_race != NULL);
+
+	Cell *l_cell;
+	unsigned int l_time;
+
+	l_cell = p_race->s_next_stopover;
+	l_time = 0;
+
+	while (l_cell != NULL) {
+		l_time += obtenir_temps(l_cell->s_stopover);
+		l_cell = l_cell->s_next_stopover;
+	}
+	return (l_time);
+}
+
+unsigned int is_circuit(Course *p_race) {
+	assert(p_race != NULL);
+
+	Escale *l_start;
+	Escale *l_end;
+	Cell *l_cell;
+
+	l_start = p_race->s_stopover;
+	l_cell = p_race;
+
+	while (l_cell->s_next_stopover != NULL)
+		l_cell = l_cell->s_next_stopover;
+
+	l_end = l_cell->s_stopover;
+
+	if (l_start != l_end)
+		return (0);
+
+	return (1);
 
 }
 
@@ -264,19 +223,14 @@ void free_race(Course *p_race) {
 unsigned int get_stopover(Course *p_race) {
 	assert(p_race != NULL);
 
-	unsigned int l_number = p_race->s_nbrStopover;
+	unsigned int l_number;
+	Cell *l_cell;
 
+	l_cell = p_race->s_next_stopover;
+
+	while (l_cell != NULL) {
+		l_cell = l_cell->s_next_stopover;
+		l_number++;
+	}
 	return (l_number);
 }
-
-unsigned int get_step(Course *p_race) {
-	assert(p_race != NULL);
-
-	unsigned int l_step = p_race->s_nbrStopover;
-
-	if (l_step > 0)
-		l_step -= 1;
-
-	return (l_step);
-}
-
