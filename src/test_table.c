27c3182caa1edfa34d71c835db4991ae07af4ba0
Timothy-Smeers <timothy.smeers@student.uliege.be>
2021-05-08 15:56:17 +0200

    test

--- src/test_table.c
+++ src/test_table.c
@@ -36,25 +36,24 @@ static void test_add_stopover() {
 	l_race = create_table_race(l_firstStopover, l_secondStopover);
 	l_race = add_stopover(l_race, l_thirdStopover, 2);
 
-
 	l_stopover = obtain_table_stopover(l_race, 0);
 
-	assert_float_equal(get_latitude(l_firstStopover),
-			get_latitude(l_stopover), 0);
+	assert_float_equal(get_latitude(l_firstStopover), get_latitude(l_stopover),
+			0);
 	assert_float_equal(get_longitude(l_firstStopover),
 			get_longitude(l_stopover), 0);
 
 	l_stopover = obtain_table_stopover(l_race, 1);
 
-	assert_float_equal(get_latitude(l_secondStopover),
-			get_latitude(l_stopover), 0);
+	assert_float_equal(get_latitude(l_secondStopover), get_latitude(l_stopover),
+			0);
 	assert_float_equal(get_longitude(l_secondStopover),
 			get_longitude(l_stopover), 0);
 
 	l_stopover = obtain_table_stopover(l_race, 2);
 
-	assert_float_equal(get_latitude(l_thirdStopover),
-			get_latitude(l_stopover), 0);
+	assert_float_equal(get_latitude(l_thirdStopover), get_latitude(l_stopover),
+			0);
 	assert_float_equal(get_longitude(l_thirdStopover),
 			get_longitude(l_stopover), 0);
 
@@ -73,15 +72,16 @@ static void test_race_time() {
 	l_thirdStopover = create_stopover(-40, 25, "Eupen"); /* @suppress("Avoid magic numbers") */
 	l_race = create_table_race(l_firstStopover, l_secondStopover);
 
-	log_time(l_secondStopover, 25); /* @suppress("Avoid magic numbers") */
-	assert_float_equal(10, race_table_time(l_race), 0);
+	l_race = ajouter_escale(l_race, l_thirdStopover, 2);
 
-	log_time(l_thirdStopover, 2);
-	add_stopover(l_race, l_thirdStopover, 2);
-	assert_float_equal(12, race_table_time(l_race), 0);
+	assert_float_equal(get_latitude(l_firstStopover), get_latitude(obtain_table_stopover(l_race, 0)), 0);
+	assert_float_equal(get_longitude(l_firstStopover), get_longitude(obtain_table_stopover(l_race, 0)), 0);
 
-	remove_table_stopover(l_race, 2);
-	assert_float_equal(10, race_table_time(l_race), 0);
+	assert_float_equal(get_latitude(l_secondStopover), get_latitude(obtain_table_stopover(l_race, 1)), 0);
+	assert_float_equal(get_longitude(l_secondStopover), get_longitude(obtain_table_stopover(l_race, 1)), 0);
+
+	assert_float_equal(get_latitude(l_thirdStopover), get_latitude(obtain_table_stopover(l_race, 2)), 0);
+	assert_float_equal(get_longitude(l_thirdStopover), obtenget_longitudeir_y(obtain_table_stopover(l_race, 2)), 0);
 
 	free_table_race(l_race);
 }
